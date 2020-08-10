b7caa18ca3d455eb082a3504edb5939dd3970909
adamnator92 <wolx5199@gmail.com>
2020-08-10 12:02:43 +0800

    initial commit

---
+++ script.sh
@@ -0,0 +1,4 @@
+docker exec -it <container-name> 
+mongo -u <your username> -p <your password> --authenticationDatabase <your database name>
+mongo -u <your username> --authenticationDatabase <your database name>
+mongodb://YourUsername:YourPasswordHere@127.0.0.1:27017/your-database-name
